c8f386f0232eeb97c6998631b19879a1547616b4
Johannes Rabauer <8188460+JohannesRabauer@users.noreply.github.com>
2025-07-05 20:17:51 +0200

    Init Spring Boot project

---
+++ src/main/java/dev/rabauer/ai_ascii_adventure/AiAsciiAdventureApplication.java
@@ -0,0 +1,13 @@
+package dev.rabauer.ai_ascii_adventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAsciiAdventureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiAsciiAdventureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/rabauer/ai_ascii_adventure/AiAsciiAdventureApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rabauer.ai_ascii_adventure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAsciiAdventureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
